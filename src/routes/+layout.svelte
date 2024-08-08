a25f6281faf9acb2a191cba351a012d6544e8555
Craig Hopgood <searaig@gmail.com>
2024-08-08 10:50:43 -0400

    initial config

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+import "../app.css";
+</script>
+
+<div class="container mx-auto">
+    <div class="mx-4 my-8">
+
+        <header class="mb-8">
+            <h2 class="text-xs uppercase">Alchemy Ethereum Bootcamp</h2>
+            <h1 class="text-3xl">Ethereum Block Explorer</h1>
+        </header>
+
+        <slot/>
+
+    </div>
+</div>
